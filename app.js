1dc6cebe9a8ee25ea511bfe7d6e1d9c6d6b1b5fe
kalvian2 <aditya.kalvium@community>
2023-12-20 10:16:26 +0530

    added comments

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Selecting necessary elements from the DOM
 const searchBox = document.querySelector('.searchBox');
 const searchBtn = document.querySelector('.searchBtn');
 const recipeContent = document.querySelector('.recipe-content');
@@ -5,16 +6,19 @@ const popularMealSection = document.getElementById('random-meal');
 const recipeContainer = document.querySelector('.recipe-container');
 const recipeCloseBtn = document.querySelector('.recipe-closeBtn');
 
+// Event listener on window load to fetch a random recipe
 window.addEventListener('load', () =>{
     fetchRecipeRandomData();
 });
-
+// Asynchronous function to fetch recipes based on a query
 const fetchRecipes = async (query)=>{
     recipeContainer.innerHTML = "<h2>Fetching Recipes...</h2>";
 try{
+     // Fetch data from the API based on the query
     const data = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${query}`);
     const response = await data.json();
     
+    // Clear the previous content and display fetched recipes
     recipeContainer.innerHTML = "";
     response.meals.forEach(meal => {
         const recipeDiv = document.createElement('div');
@@ -28,12 +32,16 @@ try{
     });
 }
 catch(error){
+            // Display an error message if there's an issue fetching recipes
+
     recipeContainer.innerHTML = "<h2>Error in Fetching Recipes...</h2>";
 }
 }
-
+// Function to fetch data for a random recipe
 const fetchRecipeRandomData = async () => {
     try {
+    // Fetch data for a random recipe
+
         const res = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
         const data = await res.json();
         renderRandomRecipes(data.meals[0]);
@@ -41,7 +49,7 @@ const fetchRecipeRandomData = async () => {
         console.error('Error in Fetching Recipes:', error);
     }
 };
-
+// Function to render details of a random recipe
 const renderRandomRecipes = (meal) => {
     popularMealSection.innerHTML = '';
     const randomMeal = document.createElement('div');
@@ -53,6 +61,7 @@ const renderRandomRecipes = (meal) => {
     <p><span>${meal.strArea}</span> Dish</p>
     <p>Belongs to <span>${meal.strCategory}</span> Category</p>
     `;
+     // Create a button to view the recipe and add an event listener
     const button = document.createElement('button');
         button.textContent = "View Recipe";
         randomMeal.appendChild(button);
@@ -62,7 +71,7 @@ const renderRandomRecipes = (meal) => {
         });
     popularMealSection.appendChild(randomMeal);
 };
-
+// Function to retrieve the list of ingredients for a recipe
 const getIngredients = (meal) =>{
     let ingredientsList = "";
     for(let i = 1; i<=20; i++){
@@ -77,6 +86,7 @@ const getIngredients = (meal) =>{
     }
     return ingredientsList;
 }
+// Function to display the recipe details in a popup
 const openRecipeBox = (meal) => {
     recipeContent.innerHTML  = `
         <h2 class="recipeName">${meal.strMeal}</h2>
@@ -86,10 +96,11 @@ const openRecipeBox = (meal) => {
     recipeContent.parentElement.style.display = "block";
 
 }
-
+// Event listener to close the recipe popup
 recipeCloseBtn.addEventListener("click", ()=>{
     recipeContent.parentElement.style.display = "none";
 })
+// Event listener for the search button to fetch recipes based on input
 searchBtn.addEventListener("click",(e)=>{
     e.preventDefault();
     const searchInput = searchBox.value.trim();
